9426eaaed12cea5e6e28f434f0304eef8feee2ba
joaquin1219 <jideguzman1219@yahoo.com>
2025-10-27 05:20:41 +0800

    added ui support for budgets

---
+++ app/src/main/java/com/mobdeve/s17/group11/smartspend/budgets/BudgetsListAdapter.java
@@ -0,0 +1,82 @@
+package com.mobdeve.s17.group11.smartspend.budgets;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.mobdeve.s17.group11.smartspend.R;
+import com.mobdeve.s17.group11.smartspend.util.FormatHelper;
+import com.mobdeve.s17.group11.smartspend.util.date.DateHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BudgetsListAdapter extends RecyclerView.Adapter<BudgetsListAdapter.budgetsListItemViewHolder> {
+
+    private final Context context;
+
+    public BudgetsListAdapter(Context context) {
+        this.context = context;
+    }
+
+    public List<BudgetsListItem> items = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public budgetsListItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new budgetsListItemViewHolder(
+                LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_expenses_list, parent, false)
+        );
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onBindViewHolder(@NonNull budgetsListItemViewHolder holder, int position) {
+        BudgetsListItem budgetsListItem = items.get(position);
+
+        holder.tvCategory.setText(budgetsListItem.budgetCategory.getDisplayName());
+
+        holder.tvDate.setText(
+                DateHelper.numericalDateTransform0(budgetsListItem.startDate) + " - " +
+                        DateHelper.numericalDateTransform0(budgetsListItem.endDate)
+        );
+
+        holder.tvPrice.setText(FormatHelper.floatToPrice(budgetsListItem.price));
+
+        holder.itemView.setOnClickListener(view -> {
+            Intent intent = new Intent(context, BudgetsEditActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+            context.startActivity(intent);
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    public static class budgetsListItemViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView tvCategory, tvDate, tvPrice;
+        public ImageButton btnDelete;
+
+        public budgetsListItemViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            tvCategory = itemView.findViewById(R.id.tv_category);
+            tvDate = itemView.findViewById(R.id.tv_date);
+            tvPrice = itemView.findViewById(R.id.tv_price);
+        }
+
+    }
+
+}
